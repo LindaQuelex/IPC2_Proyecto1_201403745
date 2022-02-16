bd232a5470076cf5bf3ee322fdac6c7cc06d2c9b
Linda Quelex <lindamadelin@gmail.com>
2022-02-15 19:31:45 -0600

    Actualización

---
+++ PROYECTO/nodopiso.py
@@ -0,0 +1,19 @@
+from ast import Attribute
+from xml.dom import minidom
+ 
+ruta = ("piso.xml")
+doc = minidom.parse(ruta)
+piso = doc.getElementsByTagName ("piso")
+
+#print("Los códigos de los pisos son:")
+#print( piso[1].attributes["nombre"].value)
+
+print ('\nLos nombres de los pisos son:')
+for elem in piso:
+    print(elem.attributes['nombre'].value)
+    
+
+print('\nLos datos son: ')
+for elem in piso:
+    print(elem.firstChild.data)
+
